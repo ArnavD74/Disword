59340409273ee13ef8edfc7bbe3999d172c6b030
Arnav Dashaputra <arugold1@gmail.com>
2022-02-03 17:25:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-//Designed by the Tangerine team, https://discord.gg/uwcgjYw or alt#0001
 const Discord = require("discord.js");
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"]
@@ -7,35 +6,15 @@ const config = require("./auth.json");
 const yawg = require('yawg');
 const checkWord = require('check-if-word'),
   words = checkWord('en');
-// function thousands_separators(num) {
-//   var num_parts = num.toString().split(".");
-//   num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-//   return num_parts.join(".");
-// }
-
 
 
 client.on("ready", () => {
-  //console.log(`Wordbot is online, with ${thousands_separators(client.users.size)} users, in ${thousands_separators(client.channels.size)} channels of ${thousands_separators(client.guilds.size)} servers.`);
   console.log(`Wordbot is online`);
   client.user.setActivity(`Wordle`, {
-    type: 'WATCHING' //PLAYING, LISTENING, WATCHING
+    type: 'WATCHING'
   });
-  client.user.setStatus('online'); //online, idle, invisible, dnd
+  client.user.setStatus('online');
 });
-
-client.on("guildCreate", guild => {
-  let channel = client.channels.get(
-    guild.channels
-    .filter(
-      c =>
-      c.permissionsFor(client.user).has("SEND_MESSAGES") &&
-      c.type === "text"
-    )
-    .map(r => r.id)[0]
-  );
-});
-
 client.on("messageCreate", async message => {
   if (message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
@@ -78,27 +57,21 @@ client.on("messageCreate", async message => {
 
     message.reply("Please enter a 5-letter word, e.g. hello");
 
-    var try1 = "";
-    var isWord = false;
-    var isFive = false;
-
     const filter = m => m.author.id === message.author.id;
     const collector = message.channel.createMessageCollector({
       filter,
       time: 5000
     });
-    collector.on('collect', m => {
-      console.log(`captured ${m.content}`);
-      while (isFive == false) {
-        if (m.content.length == 5) {
-          console.log("Is five letters.");
-          isFive = true;
-        } else {
-          message.reply("Please make sure your word is 5 letters!");
-          console.log("Is not five letters.");
-        }
-      }
-      while (isWord == false) {
+
+    var try1 = "";
+    var isWord = false;
+    var isFive = false;
+
+    while (isFive === false && isWord === false) {
+      
+      collector.on('collect', m => {
+        console.log(`captured ${m.content}`);
+        try1 = m.content;
         if ((words.check(`{m.content}`))) {
           isWord = true;
           console.log("Is english.");
@@ -106,20 +79,26 @@ client.on("messageCreate", async message => {
           message.reply("That is not an English word!")
           console.log("Is not English.");
         }
-      }
-
-      try1 = m.content;
-
-      var try1arr = try1.split("");
-      for (var i = 0; i < 5; i++) {
-        if (finalArr[i] === try1arr[i]) {
-          message.reply(`Letter ${i+1} is correct!`)
-        } else if (finalWord.includes(try1arr[i])) {
-          message.reply(`Position ${i+1} is almost correct!`)
+        if (m.content.length == 5) {
+          console.log("Is five letters.");
+          isFive = true;
+        } else {
+          message.reply("Please make sure your word is 5 letters!");
+          console.log("Is not five letters.");
         }
-      }
+      });
 
-    });
+    }
+
+    var try1arr = try1.split("");
+    for (var i = 0; i < 5; i++) {
+      if (finalArr[i] === try1arr[i]) {
+        message.reply(`Letter ${i+1} is correct!`)
+      } else if (finalWord.includes(try1arr[i])) {
+        message.reply(`Position ${i+1} is almost correct!`)
+      }
+    }
+
 
 
   }
